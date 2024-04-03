75a3b7f62ad670ca4da769d28ea10651541afb0c
Karthik tadakapalli <karthik.tadakapalli@gmail.com>
2024-04-03 15:27:40 -0600

    Added images to the code, >> add try and except for any Key Error >>changed the temperatures divided by 10 completed project

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ import requests
 API_KEY = "b1c8c4da48744135788e1ae737a551dc"
 
 
-def get_data(place, forecast_days, type_of_weather):
+def get_data(place, forecast_days=None):
 
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
@@ -12,13 +12,8 @@ def get_data(place, forecast_days, type_of_weather):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if type_of_weather == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-
-    if type_of_weather == "Sky":
-        filtered_data = [dict["weather"]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, type_of_weather="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
