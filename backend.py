ecd661ee1e61fa3630ea038d69f4747e9b211b52
Karthik tadakapalli <karthik.tadakapalli@gmail.com>
2024-04-03 14:41:08 -0600

    Added API key and added if clause for type of weather and printed get data method.

---
+++ backend.py
@@ -0,0 +1,24 @@
+import requests
+
+
+API_KEY = "b1c8c4da48744135788e1ae737a551dc"
+
+
+def get_data(place, forecast_days, type_of_weather):
+
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if type_of_weather == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+
+    if type_of_weather == "Sky":
+        filtered_data = [dict["weather"]["main"] for dict in filtered_data]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3, type_of_weather="Temperature"))
